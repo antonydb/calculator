0626540a6ab2b22114e50bcd36ec7dbacf9b3a3c
Antony Del Beato <Antony.DelBeato@thetrainline.com>
2019-03-08 17:46:37 +0000

    Remove default CRA content. *.tsx support is working.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Calculator from './components/Calculator';
+import './App.css';
+
+const App: React.StatelessComponent<{}> = () => <Calculator />;
+
+export default App;
---
+++ src/components/Calculator.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const App: React.StatelessComponent<{}> = () => <div>Hi I am a calculator</div>;
+
+export default App;
